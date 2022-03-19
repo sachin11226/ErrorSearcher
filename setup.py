210e5258435ac25ca62bfd301ec88472f5069a70
Sachin <sachin1122621003@gmail.com>
2022-03-19 19:05:44 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,9 +1,2 @@
-from setuptools import setup
-setup(name="ErrorSearcher",
-      version="0.1",
-      description="This is a simple package that just search error on broweser ",
-      long_description="This package has one function that take file path as input and store error in a string and search it on the default browser ",
-      author="Sachin",
-      author_email="sachin1122621003@gamil.com",
-      packages=['ErrorSearcher'],
-      )
+import setuptools
+setuptools.setup()
